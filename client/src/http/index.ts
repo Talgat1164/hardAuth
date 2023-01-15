8c9224b3558c1aacc351fe07df557f24df2ff368
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-15 15:32:58 +0600

    Interceptors и обновление access токена

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
-
+import {AuthResponse} from "../models/response/AuthResponse";
+import {store} from "../index";
+import {IUser} from "../models/IUser";
+  
 export const API_URL = `http://localhost:5000/api`
 
 const $api = axios.create({
@@ -19,6 +22,23 @@ $api.interceptors.request.use((config) => {
     (config.headers as any)['Authorization'] = `Bearer ${token}`
   }
   return config;
+})
+
+$api.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('НЕ АВТОРИЗОВАН')
+        }
+    }
+    throw error;
 })
 
 export default $api;
